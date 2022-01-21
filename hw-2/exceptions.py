30d7e6213188f8ba1d6ed67537f9a7b4c00f2653
Alexey Gushchin <alexey.gushchin@altenar.com>
2022-01-21 16:16:55 +0300

    Add exceptions classes

--- hw-2/exceptions.py
+++ hw-2/exceptions.py
@@ -4,3 +4,27 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+    def __init__(self, *args, **kwargs):
+        print('LowFuelError')
+
+
+pass
+
+
+class NotEnoughFuel(Exception):
+    def __init__(self, *args, **kwargs):
+        print('LowFuelError')
+
+
+pass
+
+
+class CargoOverload(Exception):
+    def __init__(self, *args, **kwargs):
+        print('LowFuelError')
+
+
+pass
