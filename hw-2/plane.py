a016ee7f3dd2fc9b9a621252e6ed47b0aeb5cab2
Alexey Gushchin <alexey.gushchin@altenar.com>
2022-01-24 11:45:48 +0300

    Add methods load cargo  and  remove all cargo  for plane

--- hw-2/plane.py
+++ hw-2/plane.py
@@ -1,3 +1,28 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from vehicle import Vehicle
+from exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    cargo = 0
+    max_cargo = 0
+
+
+def __init__(self, max_cargo):
+    self.max_cargo = max_cargo
+
+
+def load_cargo(self, cargo):
+    try:
+        if self.cargo + cargo <= self.max_cargo:
+            self.cargo += cargo
+
+    except CargoOverload:
+        CargoOverload('Cargo overload')
+
+
+def remove_all_cargo(self):
+    print(self.cargo)
+    self.cargo = 0
