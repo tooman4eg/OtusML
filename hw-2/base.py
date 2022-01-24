4781a7e8b15ea11e665a2cbb62faf0f59bb1c109
Alexey Gushchin <alexey.gushchin@altenar.com>
2022-01-24 11:03:26 +0300

    Change base Vehicle Add fields add init method add start  method

--- hw-2/base.py
+++ hw-2/base.py
@@ -1,5 +1,28 @@
 from abc import ABC
+from exceptions import LowFuelError
 
 
 class Vehicle(ABC):
-    pass
+    weight = 0
+    started = False
+    fuel = 0
+    fuel_consumption = 0
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.fuel = fuel
+        self.weight = weight
+        self.fuel_consumption = fuel_consumption
+
+    # добавьте метод start, который, если ещё не состояние started, проверяет,
+    # что топлива больше нуля, и обновляет состояние started,
+    # иначе выкидывает исключение exceptions.LowFuelError
+    def start(self, ):
+        if (self.started):
+            try:
+                if self.fuel > 0:
+                    self.started = True
+            except LowFuelError:
+                print(LowFuelError())
+
+
+pass
