80a0fcb6467ec7bcb9bd4ef22743c3e029f65ccf
Alexey Gushchin <alexey.gushchin@altenar.com>
2022-01-24 11:37:59 +0300

    Add  Car class  with set_engine  method

--- hw-2/car.py
+++ hw-2/car.py
@@ -1,3 +1,15 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+
+from base import Vehicle
+
+
+class Car(Vehicle):
+    engine = ''
+
+
+# объявите метод set_engine, который принимает в себя экземпляр объекта Engine и устанавливает на текущий экземпляр Car
+def set_engine(self, engine):
+    self.engine = engine
+    pass
